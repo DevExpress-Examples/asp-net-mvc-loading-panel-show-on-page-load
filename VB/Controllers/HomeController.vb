8937f10171eaa812db0233befb6641e5055f7330
DevExpressExampleBot <fakeEmail>
2023-11-24 12:59:41 +0400

    Source auto update [skip ci]

--- VB/Controllers/HomeController.vb
+++ VB/Controllers/HomeController.vb
@@ -1,7 +1,12 @@
-Public Class HomeController
-    Inherits System.Web.Mvc.Controller
+Imports System.Web.Mvc
 
-    Function Index() As ActionResult
-        Return View()
-    End Function
-End Class
+Namespace CS.Controllers
+
+    Public Class HomeController
+        Inherits Controller
+
+        Public Function Index() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
